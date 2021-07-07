bb884f7524ed7c3aaa568ce4d1a3c5f756d1d8a4
roosdraaijer <82209489+roosdraaijer@users.noreply.github.com>
2021-07-07 11:28:27 +0200

    Validating model
    Using provided data by Nauta to check implementation and hyperparameters for early stopping and learning rate scheduler

--- TCDF/utils.py
+++ TCDF/utils.py
@@ -48,7 +48,7 @@ class EarlyStopping():
     Early stopping to stop the training when the loss does not improve after
     certain epochs.
     """
-    def __init__(self, patience=100, min_delta=0.0001):
+    def __init__(self, patience=500, min_delta=0.0):
         """
         :param patience: how many epochs (the cumulative number of epochs in 
                          which the validation loss is not improved during the 
